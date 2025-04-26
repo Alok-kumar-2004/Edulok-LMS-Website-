88265e06a91d0be56a970902fcc8ecc858c19f89
Techie <anuragsinghm08@gmail.com>
2025-04-26 20:03:46 +0530

    client-api-correction

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const  axiosInstance = axios.create({
-    baseURL : 'https://edulok-lms-website-em87.onrender.com', 
+    baseURL : 'https://edulok-lms-website.onrender.com', 
     // baseURL : 'http://localhost:5000',
 });
 
